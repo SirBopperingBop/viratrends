2c10a93aeb3354f7612f60f8321fe32c983866bf
SirBopperingBop <matawifarookh@gmail.com>
2023-11-26 09:40:01 -0300

    egg

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -27,6 +27,7 @@ import {
 
 import routes from '../js/routes';
 import store from '../js/store';
+export const Context = React.createContext(); 
 
 const MyApp = () => {
   // Login screen demo data
@@ -62,97 +63,101 @@ const MyApp = () => {
     // Call F7 APIs here
   });
 
+  const [logInfo, setLogInfo] = useState()
+
   return (
-    <App { ...f7params }>
+    <Context.Provider value={[logInfo, setLogInfo]}>
+      <App { ...f7params }>
 
-        {/* Left panel with cover effect*/}
-        <Panel left cover dark>
+          {/* Left panel with cover effect*/}
+          <Panel left cover dark>
+            <View>
+              <Page>
+                <Navbar title="Left Panel"/>
+                <Block>Left panel content goes here</Block>
+              </Page>
+            </View>
+          </Panel>
+
+
+          {/* Right panel with reveal effect*/}
+          <Panel right reveal dark>
+            <View>
+              <Page>
+                <Navbar title="Right Panel"/>
+                <Block>Right panel content goes here</Block>
+              </Page>
+            </View>
+          </Panel>
+
+
+          {/* Views/Tabs container */}
+          <Views tabs className="safe-areas">
+            {/* Tabbar for switching views-tabs */}
+            <Toolbar tabbar icons bottom>
+              <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconMd="material:home" text="Home" />
+              <Link tabLink="#view-catalog" iconIos="f7:square_list_fill" iconMd="material:view_list" text="Catalog" />
+              <Link tabLink="#view-settings" iconIos="f7:gear" iconMd="material:settings" text="Settings" />
+            </Toolbar>
+
+            {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */}
+            <View id="view-home" main tab tabActive url="/" />
+
+            {/* Catalog View */}
+            <View id="view-catalog" name="catalog" tab url="/catalog/" />
+
+            {/* Settings View */}
+            <View id="view-settings" name="settings" tab url="/settings/" />
+
+          </Views>
+
+        {/* Popup */}
+        <Popup id="my-popup">
           <View>
             <Page>
-              <Navbar title="Left Panel"/>
-              <Block>Left panel content goes here</Block>
+              <Navbar title="Popup">
+                <NavRight>
+                  <Link popupClose>Close</Link>
+                </NavRight>
+              </Navbar>
+              <Block>
+                <p>Popup content goes here.</p>
+              </Block>
             </Page>
           </View>
-        </Panel>
+        </Popup>
 
-
-        {/* Right panel with reveal effect*/}
-        <Panel right reveal dark>
+        <LoginScreen id="my-login-screen">
           <View>
-            <Page>
-              <Navbar title="Right Panel"/>
-              <Block>Right panel content goes here</Block>
+            <Page loginScreen>
+              <LoginScreenTitle>Login</LoginScreenTitle>
+              <List form>
+                <ListInput
+                  type="text"
+                  name="username"
+                  placeholder="Your username"
+                  value={username}
+                  onInput={(e) => setUsername(e.target.value)}
+                ></ListInput>
+                <ListInput
+                  type="password"
+                  name="password"
+                  placeholder="Your password"
+                  value={password}
+                  onInput={(e) => setPassword(e.target.value)}
+                ></ListInput>
+              </List>
+              <List>
+                <ListButton title="Sign In" onClick={() => alertLoginData()} />
+                <BlockFooter>
+                  Some text about login information.<br />Click "Sign In" to close Login Screen
+                </BlockFooter>
+              </List>
             </Page>
           </View>
-        </Panel>
-
-
-        {/* Views/Tabs container */}
-        <Views tabs className="safe-areas">
-          {/* Tabbar for switching views-tabs */}
-          <Toolbar tabbar icons bottom>
-            <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconMd="material:home" text="Home" />
-            <Link tabLink="#view-catalog" iconIos="f7:square_list_fill" iconMd="material:view_list" text="Catalog" />
-            <Link tabLink="#view-settings" iconIos="f7:gear" iconMd="material:settings" text="Settings" />
-          </Toolbar>
-
-          {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */}
-          <View id="view-home" main tab tabActive url="/" />
-
-          {/* Catalog View */}
-          <View id="view-catalog" name="catalog" tab url="/catalog/" />
-
-          {/* Settings View */}
-          <View id="view-settings" name="settings" tab url="/settings/" />
-
-        </Views>
-
-      {/* Popup */}
-      <Popup id="my-popup">
-        <View>
-          <Page>
-            <Navbar title="Popup">
-              <NavRight>
-                <Link popupClose>Close</Link>
-              </NavRight>
-            </Navbar>
-            <Block>
-              <p>Popup content goes here.</p>
-            </Block>
-          </Page>
-        </View>
-      </Popup>
-
-      <LoginScreen id="my-login-screen">
-        <View>
-          <Page loginScreen>
-            <LoginScreenTitle>Login</LoginScreenTitle>
-            <List form>
-              <ListInput
-                type="text"
-                name="username"
-                placeholder="Your username"
-                value={username}
-                onInput={(e) => setUsername(e.target.value)}
-              ></ListInput>
-              <ListInput
-                type="password"
-                name="password"
-                placeholder="Your password"
-                value={password}
-                onInput={(e) => setPassword(e.target.value)}
-              ></ListInput>
-            </List>
-            <List>
-              <ListButton title="Sign In" onClick={() => alertLoginData()} />
-              <BlockFooter>
-                Some text about login information.<br />Click "Sign In" to close Login Screen
-              </BlockFooter>
-            </List>
-          </Page>
-        </View>
-      </LoginScreen>
-    </App>
+        </LoginScreen>
+      </App>
+    </Context.Provider>
   )
 }
 export default MyApp;
