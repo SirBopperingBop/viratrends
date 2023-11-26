15980a6dddf785fa3cb59f93527e1b76b28684dd
SirBopperingBop <matawifarookh@gmail.com>
2023-11-26 09:33:27 -0300

    egg

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ import 'framework7/css/bundle';
 
 // Import Icons and App Custom Styles
 import '../css/icons.css';
-import '../css/app.scss';
+import '../css/app.css';
 
 // Import App Component
 import App from '../components/app.jsx';
